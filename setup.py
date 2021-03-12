06766e5b868ef6f7e9ee4a342402abd45131efe5
Luis Manuel <riveroluismanuel9@gmail.com>
2021-03-12 13:20:45 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- codign: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = "dataparser",
+    version = "0.1",
+    license = "None",
+    packages = ["json",]
+)
