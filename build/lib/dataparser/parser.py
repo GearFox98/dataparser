708ce614b7d83414031166862beb9c93782af67d
Luis Manuel <riveroluismanuel9@gmail.com>
2021-03-15 08:38:47 -0400

    Updates

--- build/lib/dataparser/parser.py
+++ build/lib/dataparser/parser.py
@@ -3,6 +3,11 @@ import logging
 import json
 
 #TODO Set logger    
+logging.basicConfig(
+  level = logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s,"
+)
+LOGGER = logging.getLogger()
+
 
 #Path verification
 '''def gen_path(file_name):
@@ -15,8 +20,15 @@ import json
         if not os._exists(file_name):
             os.mkdir(directory)'''
 
+def _path_correction(path):
+    if path.__contains__("\\"):
+        return path.replace("\\", "/")
+    else:
+        return path
+
 def write_file(file_name: str, data: dict):
     #gen_path(file_name)
+    file_name = _path_correction(file_name)
     #Get dict
     if not type(data) == dict:
         raise TypeError("Argument: \"data\" must be dictionary type.\nwrite_file(\"file_name\", \"overwrite\" = True, \"data\" = { }")
@@ -30,6 +42,10 @@ def write_file(file_name: str, data: dict):
 
         jstream.close()
 
+        #Logger
+        log = file_name.split("/")
+        LOGGER.info(f"{log[-1]} successfully created")
+
 def read_file(file_name: str):
     jstream = open(file_name, "r")
     dt = jstream.read()
@@ -40,24 +56,56 @@ def read_file(file_name: str):
     return data
 
 #file_name = Name of the file, data = tuple to filter, returns a tuple
-def get_data(file_name: str, data: tuple):
+def get_data(file_name: str, data: tuple, as_dict = False):
     DATA = read_file(file_name)
-    if data.count == 0:
-        return tuple(DATA.values())
+    if as_dict:
+        if data.count == 0:
+            return DATA
+        else:
+            dData = dict()
+            keys = tuple(DATA.keys())
+            values = tuple(DATA.values())
+
+            for x in keys:
+                dData[x] = values[keys.index(x)]
+            
+            return dData
     else:
-        lData = list()
-        for x in data:
-            lData.append(DATA[x])
-        return tuple(lData)
+        if data.count == 0:
+            return tuple(DATA.values())
+        else:
+            lData = list()
+            for x in data:
+                lData.append(DATA[x])
+            return tuple(lData)
 
 def update_file(file_name: str, data: dict):
+    file_name = _path_correction(file_name)
     if file_name == "":
         raise ValueError("\"file_name\" must not be empty")
     elif data.__len__() == 0:
         raise ValueError("\"data\" must contain data to update")
     else:
-        DATA = read_file(file_name)
-        keys = tuple(data.keys())
-        for x in keys:
-            DATA[x] = data[x]
-        write_file(file_name, DATA)
+        try:
+            DATA = read_file(file_name)
+        except:
+            LOGGER.warning(f"No {file_name} were found, a new file will be created")
+            DATA = dict()
+        finally:
+            keys = tuple(data.keys())
+            for x in keys:
+                DATA[x] = data[x]
+            write_file(file_name, DATA)
+
+            #Logging
+            log = file_name.split("/")
+            LOGGER.info(f"{log[-1]} updated successfully")
+
+def delete_data(file_name: str):
+    file_name = _path_correction(file_name)
+    if file_name == "":
+        raise ValueError("\"file_name\" must not be empty")
+    else:
+        os.remove(file_name)
+        log = file_name.split("/")
+        LOGGER.info(f"{log[-1]} was removed")
